4c40fad221ed8bda476deb136755adf52286b0e3
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-26 19:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function takeANumber (katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
-  
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
 }
   
