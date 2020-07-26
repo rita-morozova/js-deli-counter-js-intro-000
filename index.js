8e6c35a50cb2340944631fd498433107184094f2
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-26 19:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
  if (katzDeliLine.length > 0){
+   return `The line is currently: ${i+1}. ${katzDeliLine[i]}`
     for (let i=0; i<katzDeliLine.length; i++){
-    return `The line is currently: ${i+1}. ${katzDeliLine[i]}`
+    
   }
   }else {
     return "The line is currently empty."
