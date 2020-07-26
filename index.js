b53270e68edb08623b181158d9ef8aa08dfacd09
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-26 19:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function takeANumber (katzDeliLine, name){
-  var currentLine =[];
-  let i=0
-  while (i<katzDeliLine.length-1){
-    currentLine=katzDeliLine.push(name);
-    i++
-  }
- return `Welcome ${currentLine[i]}. You are number ${i+1} in line.`
+  
 }
 
 
